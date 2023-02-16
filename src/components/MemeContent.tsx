6e9a2f444a5f4ebaa40d80f246f92bcf06dd9971
Leandro Bataglia <leandro.bataglia@gmail.com>
2023-02-16 18:29:38 -0300

    updated useState so set image

--- src/components/MemeContent.tsx
+++ src/components/MemeContent.tsx
@@ -1,13 +1,17 @@
 
-export default function MemeContent(){
+import { ImageHandler } from "./MainContent"
+
+export default function MemeContent({imageState}:ImageHandler) 
+{
+    
     return (
-        <div className="relative flex justify-center mx-3 bg-slate-400">
-            <div className="relative">
-                <img className="object-cover aspect-video" src="https://resizedimgs.vivareal.com/fit-in/870x653/named.images.sp/c224f404e77db7e5ce01081c0f219bae/%7Bdescription%7D.jpg" alt="" />
-            </div>
+        <div className="relative flex flex-col justify-center mx-3">
+            
+            <img id="Image" className="object-fit aspect-video" src={imageState} alt="" />
+            
             <div className="absolute top-0 left-0 flex flex-col w-full h-full justify-between items-center">
                 <h2 className="mt-1 uppercase text-4xl text-white font-outline-white"> Shut up </h2>
-                <h2 className=" mb-1 uppercase text-4xl text-white font-outline-white"> and take my money </h2>
+                <h2 className="mb-1 uppercase text-4xl text-white font-outline-white"> and take my money </h2>
             </div>
         </div>
     )
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,9 +2,9 @@ import "./../assets/styles/index.css"
 
 export default function NavBar() {
     return (
-        <div className="bg-gradient-to-r from-purple-900 to-purple-500  flex gap-2 px-2 py-3">
-            <img className="w-14 h-fit" src="./TrollFace.png" alt="troll face" />
-            <h1 className="text-white text-3xl my-auto">Meme Generator</h1>
+        <div className="bg-gradient-to-r from-[#672280] to-[#A626D3] flex gap-2 px-2 py-3">
+            <img className="w-12 h-fit" src="./TrollFace.png" alt="troll face" />
+            <h1 className="text-white font-bold tracking-tight text-3xl my-auto">Meme Generator</h1>
         </div>
     )
 }
